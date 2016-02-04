0b9c084a522e6451873be92e594f70c271a43c82
Sean P. Creel <creels15@comcast.net>
2016-02-03 22:28:27 -0500

    Lab Complete

--- src/main/java/edu/carrollcc/cis232/App.java
+++ src/main/java/edu/carrollcc/cis232/App.java
@@ -1,5 +1,7 @@
 package edu.carrollcc.cis232;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world!");
+        Scanner keyboard = new Scanner(System.in);
+        
+    	System.out.print("Enter a name: ");
+        String name = keyboard.nextLine();
+        
+        System.out.printf("Hello %s!\n", name);
+        
+        keyboard.close();
     }
 }
